7ca4041ec04cf3c062ef9de2ebfeb20354eeb393
Anne Thompson <annelo@microsoft.com>
2022-09-13 11:34:38 -0700

    Initial prototype

---
+++ RoundTripJsonNode/Model.Serialization.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace RoundTripJsonNode
+{
+    public partial class Model
+    {
+        // These are internal in client libraries.
+        public void Write(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("foo");
+            writer.WriteStringValue(Foo);
+            writer.WritePropertyName("bar");
+            writer.WriteStringValue(Bar);
+
+            if (UnknownProperties != null)
+            {
+                foreach (var property in UnknownProperties)
+                {
+                    property.WriteTo(writer);
+                }
+            }
+
+            writer.WriteEndObject();
+
+            // Needed for sample
+            writer.Flush();
+        }
+
+        // These are internal in client libraries.
+        public static Model Deserialize(JsonElement element)
+        {
+            Model model = new Model();
+
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("foo"))
+                {
+                    model.Foo = property.Value.GetString();
+                    continue;
+                }
+                else if (property.NameEquals("bar"))
+                {
+                    model.Bar = property.Value.GetString();
+                    continue;
+                }
+
+                // handle unknown properties
+                if (model.UnknownProperties == null)
+                {
+                    model.UnknownProperties = new List<JsonProperty>();
+                    model.UnknownProperties.Add(property);
+                }
+            }
+
+            return model;
+        }
+    }
+}
---
+++ RoundTripJsonNode/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RoundTripJsonNode;
+using System;
+using System.Text;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+// "baz" is added in v2 and starts being returned from v1 GET operations
+string jsonIn = "{ \"foo\": \"a\", \"bar\": \"b\", \"baz\": \"c\" }";
+
+Model model = Model.Deserialize(JsonDocument.Parse(jsonIn).RootElement);
+
+using var stream = new MemoryStream();
+using var writer = new Utf8JsonWriter(stream);
+model.Write(writer);
+
+string jsonOut = Encoding.UTF8.GetString(stream.ToArray());
+
+Console.WriteLine("***");
+Console.WriteLine(jsonOut);
+
+
+Console.ReadLine();
+
+
+
